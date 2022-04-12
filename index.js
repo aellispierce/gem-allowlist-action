0ddb422809ff077da3ce91b185977c550fd28fef
bettymakes <makewithbetty@gmail.com>
2022-04-11 23:03:12 -0400

    Replace async readFile with readFileSync
    Punting on async approach for now and taking a synchronous file-reading
    approach.
    Also refactoring functions to be more readable.

--- index.js
+++ index.js
@@ -7,36 +7,59 @@ async function run() {
     const gemfile = core.getInput('gemfile');
     const allowlist = core.getInput('allowlist');
 
-    const gems = fs.readFile(gemfile, 'utf8', (err, data) => {
-      if (err) {
-        // TODO: Verify how if error in this scope bubbles up to the `try` block
-        console.error(err)
-        return
-      }
+    // fs.readFile(gemfile, 'utf8', (err, data) => {
+    // const blah = fs.readFile(`${process.cwd()}/Gemfile`, 'utf8', (err, data) => {
+    //   if (err) {
+    //     // TODO: Verify how if error in this scope bubbles up to the `try` block
+    //     console.error(err)
+    //     return
+    //   }
 
-      const regexp = RegExp(/gem ["|']([^"|']*)["|']/, 'g');
-      const array = [...data.matchAll(regexp)].map(innerArr => innerArr[1]);
+    //   const regexp = RegExp(/gem ["|']([^"|']*)["|']/, 'g');
+    //   const array = [...data.matchAll(regexp)].map(innerArr => innerArr[1]);
+      
+    //   core.info(`List of gems from Gemfile: ${array}`);
 
-      return array
-    })
+    //   return array;
+    // })
 
-    core.info(`List of gems from Gemfile: ${gems}`)
+    const gemfileRaw = fs.readFileSync(`${process.cwd()}/Gemfile`, 'utf8');
+    const gems = parseGemfile(gemfileRaw);
+    console.log('aaaa', gems);
 
-    fs.readFile(allowlist, 'utf8', (err, data) => {
-      if (err) {
-        // TODO: Verify how if error in this scope bubbles up to the `try` block
-        console.error(err)
-        return
-      }
+    const allowlistRaw = fs.readFileSync(`${process.cwd()}/allowlist.json`, 'utf8');
+    const allowedGems = parseAllowlist(allowlistRaw);
+    console.log('bbbb', allowedGems);
 
-      const obj = JSON.parse(data)
-      core.info(`Allowlist data:`)
-      obj.gems.map(gem => core.info(gem))
-    })
+
+    // fs.readFile(allowlist, 'utf8', (err, data) => {
+    //   if (err) {
+    //     // TODO: Verify how if error in this scope bubbles up to the `try` block
+    //     console.error(err)
+    //     return
+    //   }
+
+    //   const obj = JSON.parse(data)
+    //   core.info(`Allowlist data:`)
+    //   obj.gems.map(gem => core.info(gem))
+    // })
 
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function parseGemfile(content) {
+  const regexp = RegExp(/gem ["|']([^"|']*)["|']/, 'g');
+  const gems = [...content.matchAll(regexp)].map(innerArr => innerArr[1]);
+
+  return gems;
+}
+
+function parseAllowlist(content) {
+  const data = JSON.parse(content)
+
+  return data.gems;
 }
 
 run();
